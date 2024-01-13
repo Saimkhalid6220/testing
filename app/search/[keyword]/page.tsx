084655d135fd883b492a831c11eb23267d647210
Saim khalid <saimkhalid6220@gmail.com>
2024-01-13 20:28:36 +0500

    bugs fixed

--- app/search/[keyword]/page.tsx
+++ app/search/[keyword]/page.tsx
@@ -2,20 +2,11 @@ import { getSearchedMovies } from "@/lib/getMovies";
 import Movie from '@/components/movies'
 import { error } from "console";
 const SearchPage = async (props:any) => {
-  console.log(props.params.keyword)
-  try{
     const searchResultls = await getSearchedMovies('search/multi',props.params.keyword);
     // console.log(searchResultls)
     return (
-      <>
-      <h1>{props.params.keyword}</h1>
       <Movie title="Your Search Results" movies={searchResultls}/>
-      </>
     )
-  }
-  catch{
-    console.error(error)
-  }
  
 }
 
