f127e95421309e36f6fd74d9120489d2f351f456
Saim khalid <saimkhalid6220@gmail.com>
2024-01-27 19:41:03 +0500

    nothing much

--- components/carousalPoster.tsx
+++ components/carousalPoster.tsx
@@ -1,54 +1,59 @@
-'use client'
+"use client";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
-  import Autoplay from "embla-carousel-autoplay";
-import BgPoster from "./bg-poster"
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import BgPoster from "./bg-poster";
 import { Genres, searchMovies } from "@/typings";
-const carousalPoster =  ({movie,genres}:{movie:searchMovies[],genres:Genres[]}) => {
-    const forPoster = movie.slice(0,3)
+const carousalPoster = ({
+  movie,
+  genres,
+}: {
+  movie: searchMovies[];
+  genres: Genres[];
+}) => {
+  const forPoster = movie.slice(0, 3);
   return (
     <div>
+      <Carousel
+        className=""
+        plugins={[
+          Autoplay({
+            delay: 8000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {forPoster.map((item: searchMovies, index: number) => (
+            <CarouselItem key={index}>
+              <BgPoster poster_path={item.backdrop_path} />
+              <div className="absolute  flex flex-col justify-end items-start z-10 top-0 text-white text-bold h-full w-full">
+                <div className="max-w-3xl ml-8 space-y-2  px-2 py-4">
+                  <h2 className="text-2xl md:text-4xl">{item.title}</h2>
+                  <p className=" text-sm hidden md:block">{item.overview}</p>
+                  <p className="text-sm text-white flex space-x-2">
+                    {item.genre_ids.map((id: number, index: number) => (
+                      <span key={index}>
+                        {genres &&
+                          genres.find((genre: Genres) => genre.id === id)?.name}
+                      </span>
+                    ))}
+                  </p>
+                  <p className="text-sm text-white">
+                    IMDB : {item.vote_average && item.vote_average.toFixed(1)}
+                  </p>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/* <CarouselPrevious /> */}
+        {/* <CarouselNext /> */}
+      </Carousel>
+    </div>
+  );
+};
 
-    <Carousel className="" 
-    plugins={[
-        Autoplay({
-          delay: 8000,
-        }),
-      ]}
-    >
-  <CarouselContent>
-    {forPoster.map((item:searchMovies) => (
-<>
-    <CarouselItem >
-        <BgPoster poster_path={item.backdrop_path}/>
-        <div  className='absolute  flex flex-col justify-end items-start z-10 top-0 text-white text-bold h-full w-full'>
-                <div key={item.id} className='max-w-3xl ml-8 space-y-2  px-2 py-4'>
-                    <h2 className='text-2xl md:text-4xl'>{item.title}</h2>
-                    <p className=' text-sm hidden md:block'>{item.overview}</p>
-                    <p className="text-sm text-white flex space-x-2">
-                      {
-                        item.genre_ids.map((id:number,index:number) =>(
-                          <span key={index}>{genres && genres.find((genre:Genres)=>genre.id===id)?.name}</span>
-                        ))
-                      }
-                    </p>
-                    <p className="text-sm text-white">IMDB : {item.vote_average && item.vote_average.toFixed(1)}</p>
-                </div>            
-        </div>
-        </CarouselItem>
-</>
-    ))}
-  </CarouselContent>
-  {/* <CarouselPrevious /> */}
-  {/* <CarouselNext /> */}
-</Carousel> 
-</div>
-
-  )
-}
-
-export default carousalPoster
+export default carousalPoster;
--- components/movies.tsx
+++ components/movies.tsx
@@ -1,64 +1,83 @@
+'use client'
 import { getPictures } from "@/lib/getMovies";
 import { Props, searchMovies } from "@/typings";
 import { BookmarkPlus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-const movies =({ title, movies }: Props) => {
-for(let i = 0; i<movies.length; i++){
-    if(movies[i].known_for  || !movies[i].backdrop_path){
-        delete movies[i];
+const movies = ({ title, movies }: Props) => {
+  for (let i = 0; i < movies.length; i++) {
+    if(!movies[i]?.poster_path){
+      delete movies[i]
     }
-}
-    return (
-        <div >
-            <div>
-                <div className="bg-white dark:bg-black">
-                    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white uppercase">{title}</h2>
+  }
+  function addBookmark(){
+    console.log("bookmark added")
+  }
+  return (
+    <div>
+      <div>
+        <div className="bg-white dark:bg-black">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white uppercase">
+              {title}
+            </h2>
 
-                        <div className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
-                            { movies.map((product: searchMovies) => (
-                                <div key={product.id} className="group relative ">
-                                        <BookmarkPlus className="top-[-8px] absolute right-4 text-[100px text-white h-8 w-8 bg-black  rounded p-1 "/>
-                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                        <Image
-                                            width={100}
-                                            height={100}
-                                            src={getPictures(product.poster_path,product.backdrop_path)}
-                                            alt="movie picture"
-                                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                        />
-                                    </div>
-                                    <div className="mt-4 flex justify-between">
-                                        <div>
-                                            <h3 className="text-sm text-gray-500">
-                                                <Link className="font-bold" href={
-                                                    {
-                                                        pathname: '/movie/' + product.id.toString(),
-                                                        query: {
-                                                            title: product.title?product.title:product.name,
-                                                            backdrop_path: product.backdrop_path,
-                                                            overview: product.overview,
-                                                            vote_average: product.vote_average.toFixed(1),
-                                                            genre_ids: product.genre_ids
-                                                        }
-                                                    }
-                                                }>
-                                                    <span aria-hidden="true" className="absolute inset-0 " />
-                                                    {product.title?product.title:product.name}
-                                                </Link>
-                                            </h3>
-                                            <p className="mt-1 text-sm text-gray-500">{product.overview && product.overview.substring(0,60)}..</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+            <div className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
+              {movies.map((product: searchMovies) => (
+                <div key={product.id} className="group relative ">
+                  <BookmarkPlus onClick={()=>addBookmark()} className="cursor-pointer z-10 top-[-8px] absolute right-4 text-[100px text-white h-8 w-8 bg-black  rounded p-1 " />
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <Image
+                      width={100}
+                      height={100}
+                      priority 
+                      src={getPictures(
+                        product.poster_path,
+                        product.backdrop_path
+                      )}
+                      alt="movie picture"
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-500">
+                        <Link
+                          className="font-bold"
+                          href={{
+                            pathname: "/movie/" + product.id.toString(),
+                            query: {
+                              title: product.title
+                                ? product.title
+                                : product.name,
+                              backdrop_path: product.backdrop_path,
+                              overview: product.overview,
+                              vote_average:product.vote_average && product.vote_average.toFixed(1),
+                              genre_ids: product.genre_ids,
+                            },
+                          }}
+                        >
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0 "
+                          />
+                          {product.title ? product.title : product.name}
+                        </Link>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.overview && product.overview.substring(0, 60)}
+                        ..
+                      </p>
                     </div>
+                  </div>
                 </div>
+              ))}
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default movies
+export default movies;
