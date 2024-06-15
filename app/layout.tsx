80ec852f941ce9ad2febd2317be1d5103b94c719
Saimkhalid6220 <99190178+Saimkhalid6220@users.noreply.github.com>
2024-06-15 20:54:37 +0500

    Version 1.00

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
         enableSystem
         disableTransitionOnChange
         >
-          <Banner/>
         <Header />
         {children}
         </ThemeProvider>
