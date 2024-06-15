0c3003fffad23787ae31c959e7919b92ad39cb91
Saimkhalid6220 <99190178+Saimkhalid6220@users.noreply.github.com>
2024-06-15 20:55:59 +0500

    Version 1.00

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -59,7 +59,7 @@ const DownloadButton = (params:any) => {
         
         <div className="m-4 text-center p-4 dark:bg-slate-900 bg-slate-200 dark:border dark:rounded-md dark:border-slate-100 flex justify-center items-center flex-col space-y-2">
         <h3 className="capitalize text-black dark:text-white">Download your movie</h3>
-        <p className="text-yellow-500 text-sm capitalize flex  items-center"><AlertTriangle className="text-sm mx-1"/>NOTE: the download link may redirect you to other website to download movies sucessfully </p>
+        <p className="text-yellow-500 text-sm capitalize flex  items-center"><AlertTriangle className="text-sm mx-1"/>NOTE: the download link may redirect you to other page to download movies sucessfully </p>
         {window.location.pathname == `/movie/${params.l_id}/download` || params.media_type == 'movie' ?(  
           
           <button onClick={(e) => downloadFile(Movie.download_link)} className="dark:bg-black dark:border dark:border-slate-200 bg-slate-900 text-white py-2 px-4 rounded-md capitalize my-4 flex items-center">Download <ArrowDownToLine className="mx-1"/></button>
