b46334581494e800b550d8a5f29b64f2592a5889
Saim khalid <saimkhalid6220@gmail.com>
2024-06-17 13:42:05 +0500

    errors

--- app/apis/getFile/route.ts
+++ app/apis/getFile/route.ts
@@ -1,26 +1,26 @@
-// pages/api/download.ts
+// app/api/download/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import axios from 'axios';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import axios from 'axios';
+import { cookies } from 'next/headers'; // Import cookies to use with Supabase
 
 const GOOGLE_DRIVE_DOWNLOAD_URL = 'https://www.googleapis.com/drive/v3/files';
 
 export async function GET(req: NextRequest) {
-  const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({cookies:() => cookieStore})
+  // Initialize the Supabase client with cookies
+  const supabase = createRouteHandlerClient({ cookies });
   const { searchParams } = new URL(req.url);
   const fileId = searchParams.get('fileId');
 
   if (!fileId) {
-    return new NextResponse(JSON.stringify({ error: 'File ID is required' }), { status: 400 });
+    return NextResponse.json({ error: 'File ID is required' }, { status: 400 });
   }
 
   // Obtain user's session from Supabase
   const { data: { session } } = await supabase.auth.getSession();
 
   if (!session || !session.access_token) {
-    return new NextResponse(JSON.stringify({ error: `User is not authenticated, acess token : ${JSON.stringify(session)}` }), { status: 401 });
+    return NextResponse.json({ error: 'User is not authenticated' }, { status: 401 });
   }
 
   const accessToken = session.access_token;
@@ -40,14 +40,12 @@ export async function GET(req: NextRequest) {
     headers.append('Content-Type', response.headers['content-type'] || 'application/octet-stream');
     headers.append('Content-Length', response.headers['content-length'] || '0');
 
-    const streamResponse = new Response(response.data, {
+    return new NextResponse(response.data, {
       headers,
       status: 200,
     });
-
-    return streamResponse;
   } catch (error) {
     console.error('Error downloading file:', error);
-    return new NextResponse(JSON.stringify({ error: 'Failed to download file' }), { status: 500 });
+    return NextResponse.json({ error: 'Failed to download file' }, { status: 500 });
   }
 }
